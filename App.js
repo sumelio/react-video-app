eef04c11a4f5ad72aa8b230075308ae2d0709129
sumelio <freddylemus@orioninfiniti.com>
2020-05-31 18:29:37 -0500

    Add video library

--- App.js
+++ App.js
@@ -29,6 +29,7 @@ import Home from './src/screens/containers/home';
 import Header from './src/sections/components/header';
 import SuggestionList from './src/videos/containers/suggestion-list';
 import CategoryList from './src/videos/containers/category-list';
+import Video from 'react-native-video';
 
 import API from './src/utils/api';
 
@@ -51,6 +52,26 @@ const App: () => React$Node = () => {
   return (
     <Home>
       <Header />
+      <View
+        style={{
+          flex: 1,
+          height: 100,
+        }}>
+        <Video
+          source={{
+            uri:
+              'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
+          }}
+          style={{
+            position: 'absolute',
+            left: 0,
+            right: 0,
+            bottom: 0,
+            top: 0,
+          }}
+          resizeMode="contain"
+        />
+      </View>
       <Text>buscador...1</Text>
       <Text>categorías</Text>
       <CategoryList list={categories} />
