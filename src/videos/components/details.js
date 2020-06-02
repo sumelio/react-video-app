f9db27234095620efc0e6eee893eccd70aff13fe
sumelio <freddylemus@orioninfiniti.com>
2020-06-02 06:11:48 -0500

    Add webview

--- src/videos/components/details.js
+++ src/videos/components/details.js
@@ -1,9 +1,33 @@
 import React from 'react';
-import {View, Text, Image, StyleSheet} from 'react-native';
+import {View, Text, Image, StyleSheet, ScrollView} from 'react-native';
+import { WebView } from 'react-native-webview';
+
+const makeHTML = id => {
+  return `
+    <style>
+      .video {
+        position: relative;
+        padding-bottom: 56.25%;
+      }
+      iframe {
+        position: absolute;
+        left: 0;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        width: 100%;
+        height: 100%;
+      }
+    </style>
+    <div class="video">
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/${id}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+    </div>
+  `;
+};
 
 function Details(props) {
   return (
-    <View>
+    <ScrollView>
       <View style={styles.top}>
         <Text>{props.title}</Text>
       </View>
@@ -18,7 +42,10 @@ function Details(props) {
           <Text style={styles.description}>{props.description_full}</Text>
         </View>
       </View>
-    </View>
+      <View style={styles.trailer}>
+        <WebView source={{html: makeHTML(props.yt_trailer_code)}} />
+      </View>
+    </ScrollView>
   );
 }
 
@@ -26,6 +53,7 @@ const styles = StyleSheet.create({
   container: {},
   trailer: {
     height: 200,
+    marginBottom: 10,
   },
   details: {
     flexDirection: 'row',
